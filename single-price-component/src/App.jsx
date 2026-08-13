9ba3ef9399aec0e696f439a96edd1fcde1c94559
Nara Mendes <283532505+nara-md@users.noreply.github.com>
2026-08-13 20:46:18 -0300

    Refactor MyComponent to accept props for dynamic content rendering

--- single-price-component/src/App.jsx
+++ single-price-component/src/App.jsx
@@ -3,7 +3,24 @@ import './App.css';
 
 function App() {
   return (
-    <MyComponent />
+    <MyComponent
+      title="Join our community"
+      subtitle="30-day, hassle-free money back guarantee"
+      description="Gain access to our full library of tutorials along with expert code reviews. 
+  Perfect for any developers who are serious about honing their skills."
+      title2="Monthly Subscription"
+      price2="&dollar;29 per month"
+      description2="Full access for less than &dollar;1 a day"
+      button2="Sign Up"
+      title3="Why Us"
+      description3="Tutorials by industry experts
+  Peer &amp; expert code review
+  Coding exercises
+  Access to our GitHub repos
+  Community forum
+  Flashcard decks
+  New videos every week"
+    />
   );
 }
 
--- single-price-component/src/components/myComponent.jsx
+++ single-price-component/src/components/myComponent.jsx
@@ -1,11 +1,23 @@
 import './myComponent.css'
 
-function  myComponent() {
+function  myComponent(props) {
     return (
         <div className="container">
-            <div className="item1">1</div>
-            <div className="item2">2</div>
-            <div className="item3">3</div>
+            <div className="item1">
+                <h1>{props.title}</h1>
+                <h2>{props.subtitle}</h2>
+                <p>{props.description}</p>
+            </div>
+            <div className="item2">
+                <h2>{props.title2}</h2>
+                <span>{props.price2}</span>
+                <p>{props.description2}</p>
+                <button>{props.button2}</button>
+            </div>
+            <div className="item3">
+                <h2>{props.title3}</h2>
+                <p>{props.description3}</p>
+            </div>
         </div>
     )
 }
